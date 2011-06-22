23ae582336d25a7c45e0f4084e61fc308a4393f3
Max Kirillov <max@max630.net>
2011-06-22 20:59:08 +0300

    removed unused lines
    just to check how many left

--- Executor.hs
+++ Executor.hs
@@ -3,9 +3,6 @@
 module Main where
 
 import Prelude hiding (succ)
-import qualified Data.Array.Unboxed as U
-import Array (Array, Ix)
-import Data.Array.IArray ((!), (//))
 import Word (Word16)
 import System (getArgs)
 import System.IO (hFlush)
@@ -16,19 +13,8 @@ import Control.Concurrent (threadDelay)
 import Data.IORef (newIORef, readIORef, writeIORef)
 import System.IO.Unsafe (unsafePerformIO)
 
-data GD = GD (Array Side PD) deriving Show -- FIXME: they really should be consistent
-
-unGD (GD v) = v
-
-data Side = ME | OP deriving (Eq, Ord, Ix, Show)
-
-other ME = OP
-other OP = ME
-
-data PD = PD {pd_vit :: U.Array Int Int, pd_field :: Array Int (Func FComb) } deriving Show
-
 data Card = I | F_zero | F_succ | F_dbl | F_get | F_put | S | K | F_inc | F_dec | F_attack | F_help | F_copy | F_revive | F_zombie
-  deriving (Eq, Ord, Ix, Show)
+  deriving (Eq, Ord, Show)
 
 pprC I = "I"
 pprC F_zero = "zero"
@@ -104,12 +90,6 @@ returnsI (Lambda _ _) = False
 castIx (Value n) | n >= 0 && n <= 255 = Just $ fromInteger (toInteger n)
 castIx _ = Nothing
 
-{- data Func =
-  Value Word16
-  | Card Card
-  | Partial Card [Func] -- Int means how many left to full call
-  deriving Show -}
-
 data FSrc = FSrc
 data FComb = FComb
 
@@ -158,123 +138,6 @@ pprF f@(Seq f1 f2) = "[" ++ concat (intersperse ", " (map pprF (getSeq f))) ++ "
 
 data Step = Move | Clean deriving (Eq, Ord, Show)
 
-
-
--- TODO: optimize it with adding diffs; cannot tolerate seeing how it rips arrays
--- FIX: the help and attack special behavior
-apply step gd side count0 func arg =
-  if count0 >= 1000
-    then err
-    else
-      case func of
-        Value _ -> err
-        Card c -> case arity c of
-          0 -> err
-          1 -> expand c [arg]
-          _ -> (gd, count, Just $ Apply (Card c) arg)
-        Apply f1 f2 | (Card c : fs) <- getCall func, length fs + 1 == arity c -> expand c (fs ++ [arg])
-        Apply f1 f2 -> (gd, count, Just $ Apply func arg)
-  where
-    expand I [x] = (gd, count, Just x)
-    expand F_succ [Value 65535] = (gd, count, Just $ Value 65535)
-    expand F_succ [Value x] = (gd, count, Just $ Value $ x + 1)
-    expand F_succ [_] = err
-    expand F_dbl [Value n] | n < 32768 = (gd, count, Just $ Value $ 2 * n)
-    expand F_dbl [Value n] = (gd, count, Just $ Value 65535)
-    expand F_dbl [_] = err
-    expand F_get [castIx -> Just i] | pd_vit pd ! i > 0 = (gd, count, Just $ Value $ fromInteger $ toInteger (pd_vit pd ! i))
-    expand F_get [_] = err
-    expand F_put [_] = (gd, count, Just $ Card I) -- not error!
-    expand S [f, g, x] =
-      case apply step gd side count f x of
-        (gd'', count'', Nothing) -> (gd'', count'', Nothing)
-        (gd'', count'', Just h) ->
-          case apply step gd'' side count'' g x of
-            (gd''', count''', Nothing) -> (gd''', count''', Nothing)
-            (gd''', count''', Just y) -> apply step gd''' side count''' h y
-    expand K [x, y] = (gd, count, Just x)
-    expand F_inc [castIx -> Just i] = (gd', count, Just $ Card I)
-      where
-        gd' = case step of
-                Move | v > 0 && v < 65535
-                      -> GD (gdV // [(side, pd {pd_vit = pd_vit pd // [(i, v + 1)]})])
-                Clean | v > 0
-                      -> GD (gdV // [(side, pd {pd_vit = pd_vit pd // [(i, v - 1)]})])
-                _ -> gd
-        v = pd_vit pd ! i
-    expand F_inc [_] = err
-    expand F_dec [castIx -> Just i0] = (gd', count, Just $ Card I)
-      where 
-        gd' = case step of
-                Move | v > 0
-                     -> GD (gdV // [(side, od {pd_vit = pd_vit od // [(i1, v - 1)]})])
-                Clean | v > 0 && v < 65535
-                     -> GD (gdV // [(side, od {pd_vit = pd_vit od // [(i1, v + 1)]})])
-                _ -> gd
-        v = pd_vit od ! i1
-        i1 = 255 - i0
-    expand F_dec [_] = err
-    expand F_help [castIx -> Just i, castIx -> Just j, Value nW]
-              | nW > 0, vi >= n
-        = (gd', count, Just $ Card I)
-      where
-        gd' = if i /= j
-                then
-                  case step of
-                    Move | v > 0
-                        -> GD (gdV // [(other side, pd {pd_vit = pd_vit pd // [(j, v'), (i, vi')]})])
-                    Clean | v > 0
-                        -> GD (gdV // [(other side, pd {pd_vit = pd_vit pd // [(j, v''), (i, vi')]})])
-                    _ -> gd
-                else
-                  case step of
-                    Move -> GD (gdV // [(other side, pd {pd_vit = pd_vit pd // [(i, vi'')]})])
-                    Clean -> GD (gdV // [(other side, pd {pd_vit = pd_vit pd // [(i, vi''')]})])
-        n = fromInteger $ toInteger nW
-        v = pd_vit pd ! j
-        vi = pd_vit pd ! i
-        v' = fromInteger $ min 65535 (toInteger v + toInteger n * 11 `div` 10)
-        v'' = fromInteger $ max 0 (toInteger v - toInteger n * 11 `div` 10)
-        vi' = fromInteger $ max 0 (toInteger vi - toInteger n)
-        vi'' = fromInteger $ min 65535 (toInteger v + toInteger n `div` 10)
-        vi''' = fromInteger $ min 65535 (toInteger v - toInteger n * 21 `div` 10)
-    expand F_help [castIx -> Just i, castIx -> Just j, Value 0] | pd_vit pd ! i > 0 = (gd, count, Just $ Card I)
-    expand F_help [_, _, _] = err
-    expand F_attack [castIx -> Just i0, castIx -> Just j, Value n] = (gd', count, Just $ Card I)
-      where
-        gd' = case step of
-                Move | v > 0
-                     -> GD (gdV // [(other side, od {pd_vit = pd_vit od // [(i1, v')]})])
-                Clean | v > 0
-                     -> GD (gdV // [(other side, od {pd_vit = pd_vit od // [(i1, v'')]})])
-                _ -> gd
-        v = pd_vit od ! i1
-        v' = fromInteger $ max 0 (toInteger v - toInteger n * 9 `div` 10)
-        v'' = fromInteger $ min 65535 (toInteger v + toInteger n * 9 `div` 10)
-        i1 = 255 - i0
-    expand F_attack [_, _, _] = err
-    expand F_copy [castIx -> Just i] = (gd, count, Just (pd_field od ! i))
-    expand F_copy [_] = err
-    expand F_revive [castIx -> Just i] = (gd', count, Just $ Card I)
-      where
-        gd' = if pd_vit pd ! i > 0
-                then gd
-                else GD (gdV // [(side, pd {pd_vit = pd_vit pd // [(i, 1)]})])
-    expand F_revive [_] = err
-    expand F_zombie [castIx -> Just i0, x] | pd_vit od ! i1 <= 0 = (gd', count, Just $ Card I)
-      where
-        gd' = GD (gdV // [(other side, od {pd_vit = pd_vit od // [(i1, -1)]
-                                          , pd_field = pd_field od // [(i1, x)]})])
-        i1 = 255 - i0
-    expand F_zombie [_] = err
-    expand c args = error ("Bad expand:" ++ pprC c ++ " " ++ show args)
-    (GD gdV) = gd
-    pd = gdV ! side
-    od = gdV ! (other side)
-    err = (gd, count, Nothing)
-    count = count0 + 1
-
-
 -- requirements for "stacked" form:
 -- stacked (Card _)
 -- stacked (Partial c (Card _ : fs)) && stacked (Partial c fs)
@@ -290,18 +153,6 @@ stack (Apply (Card c) f) = Apply (Card c) (stack f)
 stack (Apply f (Card c)) = Apply (stack f) (Card c)
 stack (Apply f1 f2@(Value v)) = stack (Apply f1 (stack f2))
 stack (Apply f1 (Apply f2 f3)) = stack (call [Card S, Apply (Card K) f1, f2, f3])
-{-
-stack (Partial c []) = error "partial with zero args!"
-stack (Partial c [f]) = Partial c [stack f]
-stack (Partial c (f : fs)) =
-  case stack f of
-    Card c' -> case stack (Partial c fs) of
-                Partial c fs' -> Partial c (Card c' : fs')
-                _ -> error "unexpected stacked form"
-    Partial c' [f'] -> stack (Partial S [f', Card c', Partial K [Partial c fs]])
-    Partial c' (f' : fs') -> stack (Partial S [f', Partial c' fs', Partial K [Partial c fs]])
-    f -> error ("Bad stacked: " ++ pprF f)
--}
 
 generator f = reverse $ gen (stack f)
   where
@@ -319,41 +170,10 @@ zero = Card F_zero
 getV = Card F_get
 get fs = call (Card F_get : fs)
 
--- FIXME: this looks to be broken; implement a languade with lambdas and fix
-killall0 = (s [Card F_succ, s [s [k [Card F_zero], k [Card F_get]], s [k [Apply (Card F_succ) (Card F_zero)], attack [Card F_zero]]]])
-killallM = s [s[s[attack [(Value 0)], k[Value 1]],s[k[getV], k[zero]]], succV]
-killallA = Lambda "n" (attack [Value 0, Var "n", Value 1]
-                      `Seq` Apply (Lazy "n" (get [Value 0])) (succ [Var "n"]))
-killallA2 = Lambda "n1" (Apply (Lambda "n" (attack [Value 0, Var "n", Value 1]
-                                  `Seq` Apply (Lazy "n" (get [Value 0])) (Var "n"))) (succ [Var "n1"]))
-
 killallA3 pos = Lambda "n" (call [Card F_help, Var "n", Value 0, Value 9984]
                         `Seq` attack [Value 0, Var "n", Value 11264]
                         `Seq` Apply (Lazy "n" (get [Value pos])) (succ [Var "n"]))
 
-killallA4 pos = Lambda "n"
-                  (Apply
-                    (Lambda "i"
-                      (call [Card F_help, Var "n", Value 0, Apply (Var "i") (Value 39)]
-                        `Seq` attack [Value 0, Var "n", Apply (Var "i") (Value 44)]
-                        `Seq` Apply (Lazy "n" (get [Value pos])) (succ [Var "n"])))
-                    (Card I) -- this must be * 2^something
-                  )
-
-killallA5 pos = Lambda "n"
-                  (Apply
-                    (Lambda "v"
-                      (call [Card F_help, Var "n", Value 0, Var "v"]
-                        `Seq` (Apply (Lambda "f" (call [Var "f", Var "v", call [Var "f", Value 1280]]))
-                                     (attack [Value 0, Var "n"]))
-                        `Seq` Apply (Lazy "n" (get [Value pos])) (succ [Var "n"])))
-                    (Value 9984)
-                  )
-
-kill base = Lambda "n" (call [Card F_help, Var "n", Value base, Value 9984]
-                        `Seq` attack [Value base, Var "n", Value 10240])
-
--- data Lang = Lambda String Lang | Var String | Func (Func FSrc) | Lazy Lang
 
 -- TODO: why I cannot put a here? report this issue to ghc
 closure :: Func FSrc -> Maybe (Func FSrc, Func FSrc)
@@ -398,8 +218,6 @@ contains v f0 = case f0 of
     Lambda v1 f -> contains v f
     Lazy v1 f -> v == v1 || contains v f
     Seq f1 f2 -> contains v f1 || contains v f2
-
--- main' = putStrLn $ concat $ map (\s -> case s of { Right x -> "2\n0\n" ++ pprC x ++ "\n"; Left x -> "1\n" ++ pprC x ++ "\n0\n" }) $ generator killall
 
 pMove slot cmd =
   case cmd of
